c5e5fe1143e2f16350db1560d506637faa7cea90
bjewell <37240673+bjewelll@users.noreply.github.com>
2018-06-05 13:32:55 -0700

    130 update

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,50 +7,12 @@ class Landing extends Component {
   render() {
     return(
       <div className="landing-grid">
-          <Card className="card_color">
-            <CardMedia>
-              <img src="./assets/images/findaworkout.jpeg"/>
-            </CardMedia>
-            <CardContent>
-            <Typography gutterBottom variant="headline" component="h2">
-           Lizard
-         </Typography>
-         <Typography component="p">
-           Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-           across all continents except Antarctica
-         </Typography>
-       </CardContent>
-       <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-          </Card>
-          <Card className="card_color">
-            <CardMedia>
-              <img src="./assets/images/logaworkout.jpeg"/>
-            </CardMedia>
-            <CardContent>
-            <Typography gutterBottom variant="headline" component="h2">
-           Lizard
-         </Typography>
-         <Typography component="p">
-           Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-           across all continents except Antarctica
-         </Typography>
-       </CardContent>
-       <CardActions>
-          <Button size="small" color="primary">
-            Share
-          </Button>
-          <Button size="small" color="primary">
-            Learn More
-          </Button>
-        </CardActions>
-          </Card>
+          <div className="card_color">
+              <img className="lp-image" src="./assets/images/findaworkout.jpeg"/>
+          </div>
+          <div className="card_color">
+              <img className="lp-image" src="./assets/images/logaworkout.jpeg"/>
+          </div>
     </div>
     )
   }
